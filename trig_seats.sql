f572586dea5db499d25c27b171cf91d40c73da97
lalalwawa <30895072+lalalwawa@users.noreply.github.com>
2017-10-30 18:07:35 +0800

    Update trig_seats.sql

--- trig_seats.sql
+++ trig_seats.sql
@@ -1,9 +1,7 @@
 /* 6.建立一個 Trigger trig_seats 檔名 trig_seats.sql 包含一個功能
   當新增一筆 放映時間, 電影代號, 廳院 到放映表格 playlist時
   參考廳院座位表, 產生(新增)指定場次的電影座次表 到 seats表格
-
   輸入 insert into playlist values ('2016-12-25 13:00', 1, 'A廳');
-
   輸出 select * from seats;
 */
 
@@ -20,13 +18,14 @@ BEGIN
 	SELECT @v_time = ptime, @v_movie = movie, @v_room = roomid
 	FROM INSERTED
 	
-	EXEC gen_seats @v_time, @v_movie, @v_room
+	EXEC gen_seats2 @v_time, @v_movie, @v_room
 	
 END 
 GO
 
 
-INSERT INTO playlist VALUES ('2016-12-25 13:00', 2, 'B')
+INSERT INTO playlist VALUES ('2016-12-25 13:00', 5, 'B廳')
 GO
 
-SELECT * FROM seats where movie = 2
+SELECT * FROM seats where movie=5
+GO
