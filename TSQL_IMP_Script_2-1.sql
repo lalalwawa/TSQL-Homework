59635c1f395bb631de645b16177776abf91de392
lalalwawa <30895072+lalalwawa@users.noreply.github.com>
2017-10-30 17:24:06 +0800

    Update TSQL_IMP_Script_2-1.sql

--- TSQL_IMP_Script_2-1.sql
+++ TSQL_IMP_Script_2-1.sql
@@ -1,5 +1,5 @@
-
-		        BEGIN
+create procedure loop99
+		    BEGIN
 			        SET @str += format(@i,'00') + '*' + format(@j,'00') +'='+ format(@i*@j,'00') + CHAR(9)
 			        SET @j += 1
 		        END
